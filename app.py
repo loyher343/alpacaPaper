6d35b7f8bed5841cee352f0254659ef661b3d8cd
loyher343 <loyhernandez59@gmail.com>
2021-01-15 05:16:25 -0600

    added buy or sell conditional

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 # @app.route('/')
 # def index():
 #     return {'yo'}
-def space():
-    return print('     ')
+def space(str):
+    return print('     '+str+'     ')
 
 @app.route('/buy_stock', methods=['POST'])
 def buy_stock():
@@ -24,26 +24,58 @@ def buy_stock():
     get_account = requests.get(ACCOUNT_URL, headers=HEADERS)
     account_response = json.loads(get_account.content)
     print(account_response)
-    space()
+    space("account")
+    acc_info = {
+        "BP" : account_response['buying_power'],
+        "regt_BP": account_response['regt_buying_power'],
+        "day_count" : account_response['daytrade_count'],
+    }
+    print(acc_info)
+    space("accountInfo")
+
+
 
     position_request = requests.get(POSITIONS_URL, headers=HEADERS)
     position_response = json.loads(position_request.content)
     print(position_response)
-    space()
+    space("position")
+    if position_response['message'] == 'position does not exist':
+        print(position_response['message'])
+    else:
+        position = {
+            "qty": position_response['qty']
+        }
+
+    
     #setup
     request = app.current_request
     webhook_message = request.json_body
     data = {
         "side": webhook_message['buy/sell'],                    # buy or sell
         "symbol": webhook_message['ticker'],
-        "qty": 100,                            
+        "qty": 1,                            
         "type": "limit",                                        # market, limit, stop, stop_limit, or trailing_stop
         "limit_price": webhook_message['close'],                # required if type is limit or stop_limit
         "time_in_force": "gtc",                                 # day, gtc, opg, cls, ioc, fok.
     }
 
     print(data)
-    space()
+    space("data")
+
+    # print(type(acc_info["regt_BP"]))
+    # print(data['limit_price'])
+    
+
+
+    if data['side'] == 'buy':
+        buy_shares = float(acc_info["regt_BP"])//float(data['limit_price'])
+        print(buy_shares)
+        data['qty'] = buy_shares
+    elif data['side'] == 'sell':
+        data['qty'] == position['qty']
+    print(data)
+
+
 
     #order request
     r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
