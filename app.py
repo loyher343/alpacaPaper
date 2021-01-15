fe8ff42d4dcc3f8bc79e4fae0c4a7c2f221157bf
loyher343 <loyhernandez59@gmail.com>
2021-01-14 21:07:02 -0600

    setting up for sides

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Chalice(app_name='tradingwiew-webhook-alerts')
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
 ORDERS_URL ="{}/v2/orders".format(BASE_URL)
+POSITIONS_URL="{}/v2/positions/SPXL".format(BASE_URL)
 HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 
 # @app.route('/')
@@ -17,30 +18,34 @@ HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 
 @app.route('/buy_stock', methods=['POST'])
 def buy_stock():
+
+    position_request = requests.get(POSITIONS_URL, headers=HEADERS)
+    position_response = json.loads(position_request.content)
+    print(position_response)
+    print('    ')
+    #setup
     request = app.current_request
     webhook_message = request.json_body
     data = {
-        "side": webhook_message['buy/sell'],
+        "side": webhook_message['buy/sell'],                    # buy or sell
         "symbol": webhook_message['ticker'],
-        "qty": 1,
-        "side": "buy",                                          # buy or sell
+        "qty": 100,                            
         "type": "limit",                                        # market, limit, stop, stop_limit, or trailing_stop
         "limit_price": webhook_message['close'],                # required if type is limit or stop_limit
         "time_in_force": "gtc",                                 # day, gtc, opg, cls, ioc, fok.
-        "order_class": "bracket",                               # simple, bracket, oco or oto.
-        "take_profit": {
-            "limit_price": webhook_message['close'] * 1.05
-        },
-        "stop_loss": {
-            "stop_price": webhook_message['close'] * 0.98,
-        }
     }
 
+    print(data)
+    print('    ')
+
+    #order request
     r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
     response = json.loads(r.content)
 
+
+
     print(response)
-    print('///////',response['id'],'////////')
+    #print('///////',response['id'],'////////')
    
     return {
         'message': 'I bought the stock',
